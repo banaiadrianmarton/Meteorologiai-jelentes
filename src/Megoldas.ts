11752f5b313ac42308a8845526e5c13a7e7608a0
Banai Adrián Márton <banai.adrian.marton@students.jedlik.eu>
2024-11-27 10:19:18 +0100

    5.feladat

--- src/Megoldas.ts
+++ src/Megoldas.ts
@@ -60,4 +60,48 @@ export default class Megoldas {
         });
         return szelcsendIdopontok;
     }
+
+    KozepHomersekletEsIngadozas(): string {
+        const telepulesek: { [key: string]: Idojaras[] } = {};
+
+        this.#idojaras.forEach(element => {
+            if (!telepulesek[element.telepules]) {
+                telepulesek[element.telepules] = [];
+            }
+            telepulesek[element.telepules].push(element);
+        });
+
+        let eredmeny = "";
+
+        for (const varosKod in telepulesek) {
+            const meresek = telepulesek[varosKod];
+
+            const idopontok = ["0100", "0700", "1300", "1900"];
+            const homerseklet: number[] = [];
+            let nincsMeres = false;
+
+            idopontok.forEach(ido => {
+                const meres = meresek.find(m => m.ido === ido);
+                if (meres) {
+                    homerseklet.push(meres.fok);
+                } else {
+                    nincsMeres = true;
+                }
+            });
+
+            if (nincsMeres) {
+                eredmeny += `${varosKod} NA; `;
+            } else {
+                const atlag = Math.round(homerseklet.reduce((osszeg, t) => osszeg + t, 0) / homerseklet.length);
+                eredmeny += `${varosKod} Középhőmérséklet: ${atlag}; `;
+            }
+
+            const maxFok = Math.max(...meresek.map(m => m.fok));
+            const minFok = Math.min(...meresek.map(m => m.fok));
+            const ingadozas = maxFok - minFok;
+            eredmeny += `Hőmérséklet-ingadozás: ${ingadozas}\n`;
+        }
+
+        return eredmeny;
+    }
 }
